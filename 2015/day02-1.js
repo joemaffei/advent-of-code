c3470ed4bc1e76a4d447f1e2b5f0d9746cd6302a
Joe Maffei <joemaffei@users.noreply.github.com>
2023-12-11 15:58:46 -0600

    2015 day02-2

--- 2015/day02-1.js
+++ 2015/day02-1.js
@@ -3,4 +3,4 @@ $("pre")
   .trim()
   .split(/\n/g)
   .map((l) => l.split(/x/g).sort((a, b) => a - b))
-  .reduce((t, [a, b, c]) => t + 2 * a + 2 * b + a * b * c, 0);
+  .reduce((t, [a, b, c]) => t + 2 * a * b + 2 * b * c + 2 * a * c + a * b, 0);
